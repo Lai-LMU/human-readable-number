ce268f3dcc83c2e7330bcbc465a64bbd1c2eae6c
Anastasia_Poleshuk <71152591+Lai-LMU@users.noreply.github.com>
2021-03-14 16:54:23 +0300

    feat: create a function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function toReadable (number) {
-  
+    let converter = require('number-to-words');
+    let result = converter.toWords(number);
+    result =result.replace("-", " ");
+    return result;
 }
